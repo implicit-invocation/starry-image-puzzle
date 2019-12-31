bcc5c1af4c2bb29eca01b1040ac38980818871bc
Duong Dat <tao@cu8-3.local>
2019-12-31 12:40:39 +0700

    ok

---
+++ precache-manifest.1dd9a3df39e77bb250411814979b7894.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e35ccf37c1e144c7633f095bd134439",
+    "url": "./index.html"
+  },
+  {
+    "revision": "052bd75f979764febdbd",
+    "url": "./static/css/main.01d54325.chunk.css"
+  },
+  {
+    "revision": "a81c7f64c68880b0247f",
+    "url": "./static/js/2.7a975201.chunk.js"
+  },
+  {
+    "revision": "d9b445fd12d8bcaca03d304ff543ec4e",
+    "url": "./static/js/2.7a975201.chunk.js.LICENSE"
+  },
+  {
+    "revision": "052bd75f979764febdbd",
+    "url": "./static/js/main.351d87c4.chunk.js"
+  },
+  {
+    "revision": "e34c15385f446ca8891d",
+    "url": "./static/js/runtime-main.c5e0e390.js"
+  }
+]);
